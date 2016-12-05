a94c16a2f9b72eb62e6588fd80c6c621ba4b1cfb
Welby McRoberts <welby.mcroberts@whmcr.com>
2016-12-05 23:34:49 +0000

    IP Networks and fixtures for this model

---
+++ web/ipam/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+CIDR_SIZES=(
+    (32,"/32 (1 Host)"),
+    (32,"/31 (2 Hosts)"),
+    (30,"/30 (4 Hosts)"),
+    (29,"/29 (8 Hosts)"),
+    (28,"/28 (16 Hosts)"),
+    (27,"/27 (32 Hosts)"),
+    (26,"/26 (64 Hosts)"),
+    (25,"/25 (128 Hosts)"),
+    (24,"/24 (256 Hosts)")
+)
+
+
+class IPNetwork(models.Model):
+    address = models.GenericIPAddressField()
+    size = models.PositiveSmallIntegerField(
+        choices=CIDR_SIZES
+    )
+    description = models.CharField(max_length=500)
+    in_use = models.BooleanField()
+
+    def __str__(self):
+        return "%s/%d" %(self.address, self.size)
